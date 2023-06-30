9a6fd63e93085b18b2e61e003d14abfbbf31f635
mjombanorman <mjomban@gmail.com>
2023-06-30 10:17:52 +0000

    Added ability to test if service is active and create user prometheus if doesnt exist.

--- snmp_exporter.py
+++ snmp_exporter.py
@@ -1,9 +1,7 @@
 import os
 import requests
+import getpass
 
-# # Prompt the user to enter the URL
-# url = input("Enter the URL to download the latest file: ")
-# tar_file = url.split("/")[-1]
 # Define the URL and file name of the tar file
 url = "https://github.com/prometheus/snmp_exporter/releases/download/v0.22.0/snmp_exporter-0.22.0.linux-amd64.tar.gz"
 tar_file = "snmp_exporter-0.22.0.linux-amd64.tar.gz"
@@ -25,31 +23,37 @@ ExecStart=/usr/local/bin/snmp_exporter --config.file="/usr/local/bin/snmp.yml"
 WantedBy=multi-user.target
 """
 
+# Check if the 'prometheus' user already exists, and create it if not
+username = "prometheus"
+try:
+    result = getpass.getuser()
+    if result != username:
+        os.system(f"useradd --system {username}")
+except Exception:
+    os.system(f"useradd --system {username}")
+
 # Download the tar file
 response = requests.get(url)
 with open(tar_file, "wb") as file:
     file.write(response.content)
 
 # Extract the contents of the tar file
-os.system(f"sudo tar xzf {tar_file}")
+os.system(f"tar xzf {tar_file}")
 
 # Copy the files to the /usr/local/bin/ folder
-os.system(f"sudo cp ./{extract_path}/snmp_exporter /usr/local/bin/snmp_exporter")
-os.system(f"sudo cp ./{extract_path}/snmp.yml /usr/local/bin/snmp.yml")
+os.system(f"cp ./{extract_path}/snmp_exporter /usr/local/bin/snmp_exporter")
+os.system(f"cp ./{extract_path}/snmp.yml /usr/local/bin/snmp.yml")
 
 # Create the service file
 with open("snmp-exporter.service", "w") as file:
     file.write(service_contents)
 
 # Move the service file to the appropriate location
-os.system("sudo mv snmp-exporter.service /etc/systemd/system/")
-
-# Create the prometheus user
-os.system("sudo useradd --system prometheus")
+os.system("mv snmp-exporter.service /etc/systemd/system/")
 
 # Reload systemd to pick up the new service file
-os.system("sudo systemctl daemon-reload")
+os.system("systemctl daemon-reload")
 
 # Enable and start the service
-os.system("sudo systemctl enable snmp-exporter.service")
-os.system("sudo systemctl start snmp-exporter.service")
+os.system("systemctl enable snmp-exporter.service")
+os.system("systemctl start snmp-exporter.service")
